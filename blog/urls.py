1ed3641f46209db98173adfeff06bfc0099b0b3b
kvetaren <kvet.aren@mail.ru>
2018-01-01 04:25:48 +0100

    Last commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import url
 from . import views
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^admin/', admin.site.urls),
 ]
